850aa7d4a5ded7d4e383da0dbbb20e0afa3d59eb
DhruvilSavalia <109803967+DhruvilSavalia@users.noreply.github.com>
2022-09-27 06:38:06 -0700

    Add files via upload

---
+++ Tasks/info.java
@@ -0,0 +1,10 @@
+class HelloWorld 
+{
+   public static void main(String[] args)
+  {
+    System.out.println("My name is:Dhruvil Savalia");
+    System.out.println("My birthdate is:3/10/2003");
+    System.out.println("My age is:20");
+    System.out.println("My address is:280,chandrapark part-2,  streetno-18, opposite reliance mall, near bigbazaar 150ft ring road,rajkot");
+  }
+} 
