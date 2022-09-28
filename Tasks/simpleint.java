1e1f73878ac670b5cb0c04a80bd63eee1b7c0e2d
DhruvilSavalia <109803967+DhruvilSavalia@users.noreply.github.com>
2022-09-28 00:07:43 -0700

    Add files via upload

---
+++ Tasks/simpleint.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Simple_Interest
+{
+    public static void main(String[] args)
+    {
+       Scanner sc = new Scanner(System.in);
+       int p,r,t,si;
+       System.out.println("Enter the principle:");
+       p = sc.nextInt();
+       System.out.println("Enter the rate:");
+       r = sc.nextInt();
+       System.out.println("Enter the time:");
+       t = sc.nextInt();
+       System.out.println("Simple Interest is:");
+       System.out.println(si=p*r*t/100);
+    }
+}
