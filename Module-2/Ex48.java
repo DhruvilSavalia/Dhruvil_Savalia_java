07b0536639ca58f182fef42bc99c17bb8d308ed9
DhruvilSavalia <109803967+DhruvilSavalia@users.noreply.github.com>
2023-03-30 06:25:05 -0700

    Add files via upload

---
+++ Module-2/Ex48.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Ex48 
+{
+	public static void main(String[] args) 
+	{
+		ArrayList<Integer> l = new ArrayList<>();
+		l.add(1);
+		l.add(5);
+		l.add(8);
+		l.add(3);
+		l.add(10);
+		l.add(15);
+		System.out.println(l);
+		System.out.println("Third index number is :"+l.get(2));
+		
+		
+	}
+
+}
