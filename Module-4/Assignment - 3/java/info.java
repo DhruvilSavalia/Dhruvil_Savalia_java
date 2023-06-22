38ca450c5b4fbf184c46b977cb86cb89048a0b5b
DhruvilSavalia <109803967+DhruvilSavalia@users.noreply.github.com>
2023-06-22 01:02:36 -0700

    Add files via upload

---
+++ Module-4/Assignment - 3/java/info.java
@@ -0,0 +1,36 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MyForm")
+public class info extends HttpServlet
+{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//super.doGet(req, resp);
+		
+		String Fname = req.getParameter("fname");
+		String Lname = req.getParameter("lname");
+		String Email = req.getParameter("email");
+		String Mobile = req.getParameter("mobile");
+		String Gender = req.getParameter("gender");
+		String Password = req.getParameter("password");
+		
+		resp.setContentType("text/html");
+		
+		System.out.println("Fname :"+Fname);
+		System.out.println("Lname :"+Lname);
+		System.out.println("Email :"+Email);
+		System.out.println("Mobile :"+Mobile);
+		System.out.println("Gender :"+Gender);
+		System.out.println("Password :"+Password);
+		
+	}
+	
+
+}
---
+++ Module-4/Assignment - 3/webapp/index.jsp
@@ -0,0 +1,40 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+
+
+
+<html>
+<head>
+<title>Student Registration Form</title>
+
+
+</head>
+
+
+<body>
+
+
+<br>
+<br>
+<form action="MyForm">
+First name: <input type="text" name="fname"><br>
+Last name:  <input type="text" name="lname"><br>
+Email:      <input type="text" name="email"><br>
+Mobile:     <input type="text" name="mobile"><br>
+Gender:     <input type="text" name="gender"><br>
+Password:   <input type="password" name="password"><br>
+
+<input type="submit" value="Submit">
+
+
+</form>
+</body>
+</html>
